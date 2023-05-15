82c7a25162171a1e8229e4d43d41f2d053333867
shubh-37 <shubh.arya37@gmail.com>
2023-05-15 22:51:21 +0530

    wrapped product context around app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import {BrowserRouter as Router } from "react-router-dom";
+import ProductContProvider from "./contexts/ProductContProvider";
 
 // Call make Server
 makeServer();
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ProductContProvider>
+          <App />
+      </ProductContProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
