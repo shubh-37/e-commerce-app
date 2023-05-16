9aa83ab1d3e3b393e0a5f5721caad3f5b7ce5e37
shubh-37 <shubh.arya37@gmail.com>
2023-05-16 23:19:29 +0530

    multiple fetch calls to get product and categories

--- src/contexts/ProductContProvider.jsx
+++ src/contexts/ProductContProvider.jsx
@@ -1,29 +1,70 @@
-import { createContext, useEffect, useReducer } from "react";
-
+import { createContext, useEffect, useReducer, useState } from "react";
+import reducer from "../Reducer";
 export const productContext = createContext();
 
 export default function ProductContProvider({ children }){
-    function reducer(){
-
-    }
     const [state, dispatch] = useReducer(reducer, {
-        
-    })
+        allProducts: [],
+        refData: [],
+        categories: [],
+        wishListItems: [],
+        cartItems: []
+    });
+
+    const [isLoading, setLoading] = useState(false);
+
     async function getProducts(){
         try {
             const response = await fetch("/api/products");
-            if(response.status == 200){
+            if(response.status === 200){
                 const data = await response.json();
-                console.log(data.products);
+                state.allProducts = data.products;
+                state.refData = data.products;
             }
         } catch (error) {
             console.log(error);
         }
     }
+
+    async function getCategory(){
+        setLoading(true);
+        try {
+            const response = await fetch("/api/categories");
+            if(response.status === 200){
+                const data = await response.json();
+                state.categories = data.categories;
+            }
+        } catch (error) {
+            console.log(error);   
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    async function getCartItems(){
+        try {
+            const response = await fetch("/api/user/cart")
+        } catch (error) {
+            
+        }
+    }
+
+    async function addToCart(){
+        try {
+            const response = await fetch()
+        } catch (error) {
+            
+        }
+    }
+
+    function showCategoryProd(val){
+        dispatch({type: "CATEGORY", payload: val})
+    }
     useEffect(() => {
         getProducts();
+        getCategory();
     } , [])
     return (
-        <productContext.Provider>{ children }</productContext.Provider>
+        <productContext.Provider value={{state, showCategoryProd, isLoading}}>{ children }</productContext.Provider>
     )
 }
