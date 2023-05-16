46b8a5c8b8a9afc69667f3dacb15775bcb28a354
shubh-37 <shubh.arya37@gmail.com>
2023-05-16 23:20:29 +0530

    displayed all the categories coming from API

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,3 +1,26 @@
+import { useContext } from "react";
+import {Link} from "react-router-dom";
+import { productContext } from "../contexts/ProductContProvider";
+
 export default function LandingPage(){
-    
+    const { state, showCategoryProd, isLoading } = useContext(productContext);
+    return (
+        <div>
+            <h1>{isLoading ? "Fetching products for you...": ""}</h1>
+            <ul>
+                {
+                    state?.categories.map(item => (
+                        <li key={item.id}>
+                            <button onClick={() => showCategoryProd(item.categoryName)}>
+                                <Link to ="/products">
+                                <h1>{item.categoryName}</h1>
+                                <p>{item.description}</p>
+                                </Link>
+                            </button> 
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 }
