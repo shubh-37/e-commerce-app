69007f4e316d364d6a924fc35147cc9f2d707e9b
shubh-37 <shubh.arya37@gmail.com>
2023-05-16 23:18:43 +0530

    removed unused useState variable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,26 @@ import Badge from "@material-ui/core/Badge";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import SearchIcon from '@material-ui/icons/Search';
-import { useState } from "react";
 import {NavLink} from "react-router-dom";
 export default function Header(){
-    const [itemCount, setItemCount] = useState(1);
     return(
         <div className="header">
-            <h3>MyShoppingSite</h3>
+            <NavLink to ="/"><h3>Book IT</h3></NavLink>
             <Badge color="secondary">
                     <SearchIcon />{" "}
             </Badge>
             <input type="text" name="" id="" placeholder="Search"/>
             <button>Login</button>
             <div>
-                <Badge color="secondary" badgeContent={itemCount}>
+                <Badge color="secondary">
                     <NavLink to="/cart"><ShoppingCartIcon /></NavLink>
                 </Badge>
-                <Badge color="secondary" badgeContent={itemCount}>
+                <Badge color="secondary">
                     <NavLink to="/wishlist"><FavoriteBorderIcon /></NavLink>
                 </Badge>
             </div>
         </div>
     )
 }
+
+//badgeContent={itemCount}
