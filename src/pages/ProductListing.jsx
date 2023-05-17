315583524a2b5f44b8093793479082d4ddf48665
shubh-37 <shubh.arya37@gmail.com>
2023-05-17 15:21:48 +0530

    click handler to add items to cart

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 import { productContext } from "../contexts/ProductContProvider";
 
 export default function ProductListing(){
-    const {state} = useContext(productContext);
-    console.log(state.allProducts)
+    const {state, handleCart} = useContext(productContext);
     return (
         <div>
             <ul>
@@ -14,7 +13,7 @@ export default function ProductListing(){
                             <Link to={`/product/${item.id}`}><img src="https://picsum.photos/150/150" alt="prod-img" /></Link>
                             <h3>{item.title}</h3>
                             <p>${item.price}</p>
-                            <button>Add to cart</button>
+                            <button onClick={() => handleCart(item)}>Add to cart</button>
                             <button>Add to wishlist</button>
                         </li>
                     ))
