27cdd82634f31eeca76b7c7a3bc3eaa91182d2af
shubh-37 <shubh.arya37@gmail.com>
2023-05-19 19:44:55 +0530

    added necessary inputs on sign up

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,3 +1,26 @@
 export default function Signup(){
-    
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="">
+                    Enter your FirtName: <input type="text" name="" id="" />
+                </label>
+                <label htmlFor="">
+                    Enter your LastName: <input type="text" name="" id="" />
+                </label>
+                <label htmlFor="">
+                    Enter your email ID: <input type="email" name="" id="" />
+                </label>
+                <label htmlFor="">
+                    Enter your password: <input type="password" name="" id="" />
+                </label>
+                <label htmlFor="">
+                    Re-enter your password: <input type="password" />
+                </label>
+                <button type="submit">Sign Up</button>
+            </form>
+            
+
+        </div>
+    )
 }
