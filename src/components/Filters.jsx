98f1044712b56f965c67b09e970c63a7c63a718f
shubh-37 <shubh.arya37@gmail.com>
2023-05-22 02:24:16 +0530

    added checked property in checkboxes and added min max value in range

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { productContext } from "../contexts/ProductContProvider"
 
 export default function Filter(){
-    const { sortHandler, categoryHandler, ratingHandler } = useContext(productContext);
+    const { sortHandler, categoryHandler, ratingHandler, state } = useContext(productContext);
     return (
         <div>
             <label htmlFor="">
@@ -12,18 +12,18 @@ export default function Filter(){
             </label>
             <label htmlFor="">
                 Category: 
-                <input type="checkbox" onChange={() => categoryHandler()}name="" id="" value="fiction"/> Fiction
-                <input type="checkbox" onChange={() => categoryHandler()}name="" id="" value="nonFiction"/> Non-Fiction
-                <input type="checkbox" onChange={() => categoryHandler()}name="" id="" value="horror"/> Horror
+                <input type="checkbox" onChange={() => categoryHandler("fiction")}name="" id="" value="fiction" checked={state.category.includes("fiction")}/> Fiction
+                <input type="checkbox" onChange={() => categoryHandler("non-fiction")}name="" id="" value="nonfiction" checked={state.category.includes("non-fiction")}/> Non-Fiction
+                <input type="checkbox" onChange={() => categoryHandler("horror")}name="" id="" value="horror"checked={state.category.includes("horror")}/> Horror
             </label>
             <label htmlFor="">
-                Price Range: <input type="range" name="" id="" min="200" max="1000" step= "100"/>
+                Price Range: <span>200</span><input type="range" name="" id="" min="200" max="500" step= "50"/><span>500</span>
             </label>
             <label htmlFor="">
                 Ratings: 
                 <input type="radio" onChange={() => ratingHandler("5")} name="rating" id="" value="5"/> 5 Stars
                 <input type="radio" onChange={() => ratingHandler("4")} name="rating" id="" value="4"/> 4 Stars
-                <input type="radio" onChange={() => ratingHandler("3")} name="rating" id="" value="3"/> 4 Stars
+                <input type="radio" onChange={() => ratingHandler("3")} name="rating" id="" value="3"/> 3 Stars
                 <input type="radio" onChange={() => ratingHandler("2")} name="rating" id="" value="2"/> 2 Stars
             </label>
         </div>
