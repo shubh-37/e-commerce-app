6442714f2e9a49e683fe033ef5b9f415cd610683
shubh-37 <shubh.arya37@gmail.com>
2023-05-22 02:22:24 +0530

    rendering cart and wishlist items

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,3 +1,19 @@
-export default function Cart(){
+import { useContext } from "react";
+import { productContext } from "../contexts/ProductContProvider";
 
+export default function Cart(){
+    const { state } = useContext(productContext);
+    return (
+        <div>
+            <ul>
+                {
+                    state.cartItems.map(item => (
+                        <li>
+                            {item.title}
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 }
--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -1,3 +1,20 @@
+import { useContext } from "react";
+import { productContext } from "../contexts/ProductContProvider";
+
 export default function Wishlist(){
-    
+    const {state} = useContext(productContext);
+
+    return (
+        <div>
+            <ul>
+                {
+                    state?.wishlistItems?.map(item => (
+                        <li>
+                            {item.title}
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 }
