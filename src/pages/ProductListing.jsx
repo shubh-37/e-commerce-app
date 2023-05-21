e8e94e0069f2a334db86dde049e03e7d05713ed4
shubh-37 <shubh.arya37@gmail.com>
2023-05-22 02:26:09 +0530

    added new properties of product and wishlist handler

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -4,7 +4,7 @@ import Filter from "../components/Filters";
 import { productContext } from "../contexts/ProductContProvider";
 
 export default function ProductListing(){
-    const {ratedProd, handleCart} = useContext(productContext);
+    const {ratedProd, handleCart, handleWishlist} = useContext(productContext);
     return (
         <div>
             <Filter />
@@ -13,10 +13,12 @@ export default function ProductListing(){
                     ratedProd?.map((item) => (
                         <li key={item.id}>
                             <Link to={`/product/${item.id}`}><img src="https://picsum.photos/150/150" alt="prod-img" /></Link>
-                            <h3>{item.title}</h3>
-                            <p>${item.price}</p>
+                            <h3>Title: {item.title}</h3>
+                            <p>Price: Rs {item.price}</p>
+                            <p>Genre: {item.categoryName}</p>
+                            <p>Rating: {item.rating} stars</p>
                             <button onClick={() => handleCart(item)}>Add to cart</button>
-                            <button>Add to wishlist</button>
+                            <button onClick={() => handleWishlist(item)}>Add to wishlist</button>
                         </li>
                     ))
                 }
