da462c89716f6485e47db5df6af7fb4ed4f3fc22
shubh-37 <shubh.arya37@gmail.com>
2023-05-21 15:07:01 +0530

    refact: now mapping and rendering over the filtered Data

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -4,13 +4,13 @@ import Filter from "../components/Filters";
 import { productContext } from "../contexts/ProductContProvider";
 
 export default function ProductListing(){
-    const {state, handleCart} = useContext(productContext);
+    const {ratedProd, handleCart} = useContext(productContext);
     return (
         <div>
             <Filter />
             <ul>
                 {
-                    state?.allProducts.map((item) => (
+                    ratedProd?.map((item) => (
                         <li key={item.id}>
                             <Link to={`/product/${item.id}`}><img src="https://picsum.photos/150/150" alt="prod-img" /></Link>
                             <h3>{item.title}</h3>
