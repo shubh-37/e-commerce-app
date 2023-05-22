2cf42792ebbc1e4ec277361ab214a828ce10114e
shubh-37 <shubh.arya37@gmail.com>
2023-05-22 14:35:42 +0530

    added images to categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -11,17 +11,20 @@ export const categories = [
     categoryName: "fiction",
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
+    src: "https://i.ibb.co/SX0qnNy/shri-q-Er-Pvf-IV4-unsplash-1.jpg"
   },
   {
     _id: uuid(),
     categoryName: "non-fiction",
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    src: "https://i.ibb.co/x1YzHd5/shiromani-kant-Tg-ILZ-c06-E-unsplash.jpg"
   },
   {
     _id: uuid(),
     categoryName: "horror",
     description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+      "Meant to cause discomfort and fear for both the character and readers. Beware might cause nightmares.",
+      src: "https://i.ibb.co/ggcJdj3/stormseeker-r-X12-B5u-X7-QM-unsplash.jpg" 
   },
 ];
