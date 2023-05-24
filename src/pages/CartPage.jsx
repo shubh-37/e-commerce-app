d2f2c8b333beb6e1fa281c841ecccd35969bc17d
shubh-37 <shubh.arya37@gmail.com>
2023-05-24 20:00:19 +0530

    feat: pricing details

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import { productContext } from "../contexts/ProductContProvider";
 
 export default function Cart(){
-    const { state, removeItem, incrementItem, decrementItem } = useContext(productContext);
+    const { state, removeItem, incrementItem, decrementItem, handleWishlist } = useContext(productContext);
+    const finalPrice = state.cartItems.reduce((acc, item) => acc = acc + item.price * item.qty,0);
+    const foundWL = (prodName) => state.wishlistItems.find(({title}) => title === prodName);
     return (
         <div>
             <h2>My cart ({state.cartItems.length})</h2>
@@ -14,11 +16,18 @@ export default function Cart(){
                             {item.price}
                             <button onClick={() => incrementItem(item._id)}>+</button>{item.qty}<button onClick={() => decrementItem(item._id)}>{ item.qty === 0 ? removeItem(item._id) :"-"}</button>
                             <button onClick={() => removeItem(item._id)}>Remove from cart</button>
-                            <button>Move to wishlist</button>
+                            <button onClick={() => {removeItem(item._id);handleWishlist(item)}} disabled={foundWL(item.title)} className="wl-btn">{foundWL(item.title) ? "Added to wishlist" : "Move to wishlist"}</button>
                         </li>
                     ))
                 }
             </ul>
+            <div className="pricing">
+                <h3>Price Details</h3>
+                <p>Price: {finalPrice}</p>
+                <p>Delivery charges: 100</p>
+                <p>Total amount: {finalPrice+100}</p>
+                <button>Place order</button>
+            </div>
         </div>
     )
 }
