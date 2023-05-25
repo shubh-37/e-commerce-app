f9d9d1fe691f444d62c0a7565d1b4af943d05ceb
shubh-37 <shubh.arya37@gmail.com>
2023-05-25 23:26:00 +0530

    refact: image width changed

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -17,7 +17,7 @@ export default function ProductListing(){
                 {
                     ratedProd?.map((item) => (
                         <li key={item.id} className="prod-item">
-                            <Link to={`/product/${item.id}`}><img src="https://picsum.photos/150/150" alt="prod-img" /></Link>
+                            <Link to={`/product/${item.id}`}><img src="https://picsum.photos/215/150" alt="prod-img" /></Link>
                             <h3>Title: {item.title}</h3>
                             <p>Price: Rs {item.price}</p>
                             <p>Genre: {item.categoryName}</p>
