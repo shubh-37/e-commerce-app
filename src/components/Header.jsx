540c19b8924ec2ea7e341d25ea52cf62abfd96fb
shubh-37 <shubh.arya37@gmail.com>
2023-05-25 23:23:58 +0530

    search handler for taking inputs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,18 @@ import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import SearchIcon from '@material-ui/icons/Search';
 import {Link, NavLink} from "react-router-dom";
+import { useContext } from "react";
+import { productContext } from "../contexts/ProductContProvider";
+
 export default function Header(){
+    const {searchHandler} = useContext(productContext);
     return(
         <div className="header">
             <NavLink to ="/"><h3>Book IT</h3></NavLink>
             <Badge color="secondary">
                     <SearchIcon />{" "}
             </Badge>
-            <input type="text" name="" id="" placeholder="Search"/>
+            <input onChange={(e) => searchHandler(e)} type="text" name="" id="" placeholder="Search"/>
             <Link to="/login"><button>Login</button></Link>
             <Link to="/signup"><button>Sign Up</button></Link>
             <div>
