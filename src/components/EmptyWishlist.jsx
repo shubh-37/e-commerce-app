7fb3657a13c4823c9c90e7465c8f1937344cfe12
shubh-37 <shubh.arya37@gmail.com>
2023-05-27 16:35:18 +0530

    component for displaying empty cart/WL

---
+++ src/components/EmptyWishlist.jsx
@@ -0,0 +1,9 @@
+export default function EmptyWishlist(){
+    return (
+        <div className="empty-cart">
+            <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/mywishlist-empty_39f7a5.png" alt="" />
+            <h3>Empty Wishlist!</h3>
+            <p>You have no items in your wishlist. Start adding!</p>
+        </div>
+    )
+}
