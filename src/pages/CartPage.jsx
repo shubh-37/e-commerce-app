b79b3f12dd05f4d54ce78104e62886225c606b61
shubh-37 <shubh.arya37@gmail.com>
2023-05-27 16:34:23 +0530

    added empty cart and WL component

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import EmptyCart from "../components/EmptyCart";
 import { productContext } from "../contexts/ProductContProvider";
 
 export default function Cart(){
@@ -6,28 +7,32 @@ export default function Cart(){
     const finalPrice = state.cartItems.reduce((acc, item) => acc = acc + item.price * item.qty,0);
     const foundWL = (prodName) => state.wishlistItems.find(({title}) => title === prodName);
     return (
-        <div>
-            <h2>My cart ({state.cartItems.length})</h2>
-            <ul>
-                {
-                    state.cartItems.map(item => (
-                        <li key={item.id}>
-                            {item.title}
-                            {item.price}
-                            <button onClick={() => incrementItem(item._id)}>+</button>{item.qty}<button onClick={() => decrementItem(item._id)}>{ item.qty === 0 ? removeItem(item._id) :"-"}</button>
-                            <button onClick={() => removeItem(item._id)}>Remove from cart</button>
-                            <button onClick={() => {removeItem(item._id);handleWishlist(item)}} disabled={foundWL(item.title)} className="wl-btn">{foundWL(item.title) ? "Added to wishlist" : "Move to wishlist"}</button>
-                        </li>
-                    ))
-                }
-            </ul>
-            {state.cartItems.length !== 0 && <div className="pricing">
-                <h3>Price Details</h3>
-                <p>Price: {finalPrice}</p>
-                <p>Delivery charges: 100</p>
-                <p>Total amount: {finalPrice+100}</p>
-                <button>Place order</button>
-            </div>}
+        <>
+        { state.cartItems.length === 0 ? <EmptyCart /> : 
+            <div>
+                <h2 className="heading-cart">My cart ({state.cartItems.length})</h2>
+                <ul>
+                    {
+                        state.cartItems.map(item => (
+                            <li key={item.id}>
+                                {item.title}
+                                {item.price}
+                                <button onClick={() => incrementItem(item._id)}>+</button>{item.qty}<button onClick={() => decrementItem(item._id)}>{ item.qty === 0 ? removeItem(item._id) :"-"}</button>
+                                <button onClick={() => removeItem(item._id)}>Remove from cart</button>
+                                <button onClick={() => {removeItem(item._id);handleWishlist(item)}} disabled={foundWL(item.title)} className="wl-btn">{foundWL(item.title) ? "Added to wishlist" : "Move to wishlist"}</button>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <div className="pricing">
+                    <h3>Price Details</h3>
+                    <p>Price: {finalPrice}</p>
+                    <p>Delivery charges: 100</p>
+                    <p>Total amount: {finalPrice+100}</p>
+                    <button>Place order</button>
+                </div>
         </div>
+    }
+    </>
     )
 }
--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -1,24 +1,31 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import EmptyWishlist from "../components/EmptyWishlist";
 import { productContext } from "../contexts/ProductContProvider";
-
+import "../css/wishlist.css"
 export default function Wishlist(){
     const {state, handleCart} = useContext(productContext);
     const foundC = (prodName) =>  state.cartItems.find(({title}) => title === prodName);
     return (
-        <div>
-            <h2>My Wishlist({state.wishlistItems.length})</h2>
-            <ul>
-                {
-                    state?.wishlistItems?.map(item => (
-                        <li>
-                            {item.title}
-                            {item.price}
-                            {foundC(item.title) ?<Link to="/cart" className="go-btn">Visit cart</Link> : <button className ="add-btn"onClick={() => handleCart(item)}>Move to cart</button>}
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
+        <>
+            {
+                state.wishlistItems.length === 0 ? <EmptyWishlist /> : 
+                <div className="wishlist">
+                    <h2 className="heading-wl">My Wishlist({state.wishlistItems.length})</h2>
+                    <ul>
+                        {
+                            state?.wishlistItems?.map(item => (
+                                <li>
+                                    {item.title}
+                                    {item.price}
+                                    {foundC(item.title) ?<Link to="/cart" className="go-btn">Visit cart</Link> : <button className ="add-btn"onClick={() => handleCart(item)}>Move to cart</button>}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            }
+        </>
+        
     )
 }
