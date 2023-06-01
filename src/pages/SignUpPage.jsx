6905ee31b4a8c4eec24a5d2cc1ca300442359a66
shubh-37 <shubh.arya37@gmail.com>
2023-06-01 17:04:01 +0530

    added class to elements

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"
 export default function Signup(){
     return (
-        <div>
-            <form action="">
+        <div className="login-parent">
+            
+            <form action="" className="login-sub">
+                <h2>Signup</h2>
                 <label htmlFor="">
-                    Enter your FirtName: <input type="text" name="" id="" />
+                    Enter your FirstName: <input type="text" name="" id="" />
                 </label>
                 <label htmlFor="">
                     Enter your LastName: <input type="text" name="" id="" />
@@ -18,6 +21,7 @@ export default function Signup(){
                     Re-enter your password: <input type="password" />
                 </label>
                 <button type="submit">Sign Up</button>
+                <Link to="/login"> Already have an account? Login.</Link>
             </form>
             
 
