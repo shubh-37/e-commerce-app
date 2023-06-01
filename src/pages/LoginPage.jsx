114467dfbc234e3db6146564b49aa2c3b31c0e94
shubh-37 <shubh.arya37@gmail.com>
2023-06-01 17:03:34 +0530

    added class to elements

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,25 @@
 import { useContext } from "react"
 import { productContext } from "../contexts/ProductContProvider"
+import  {Link} from "react-router-dom";
+import "../css/login.css";
 
 export default function Login(){
     const { testLogin } = useContext(productContext);
     return (
-        <div>
-            <label htmlFor="">
-                Email Address: <input type="text" name="" id="" />
-            </label>
-            <label htmlFor="">
-                Password: <input type="password" name="" id="" />
-            </label>
-            <button>Sign In</button>
-            <button onClick={() => testLogin()}>Login as test user</button>
+        <div className="login-parent">
+            <div className="login-sub">   
+                <h2>Login</h2>
+                <label htmlFor="" className="email">
+                    Email Address <br/><input type="text" name="" id="" className="email-inp" placeholder="example@gmail.com"/>
+                </label>
+                <label htmlFor="" className="pw">
+                    Password <br/><input type="password" name="" id="" className="pw-inp" placeholder="shubh@123" />
+                </label>
+                <button className="signin-btn">Sign In</button>
+                <Link to="/signup" className="signup-link">Don't have an account? Sign Up.</Link>
+                <button onClick={() => testLogin()} className="test-btn">Login as test user</button>
+            </div>
+            
         </div>
     )
 }
