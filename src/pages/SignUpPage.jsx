0ed74afcca1e01b1dc36d71b31d612d450cf99d4
shubh-37 <shubh.arya37@gmail.com>
2023-06-04 15:51:40 +0530

    sign up functionality on working

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,30 +1,98 @@
-import { Link } from "react-router-dom"
-export default function Signup(){
-    return (
-        <div className="login-parent">
-            
-            <form action="" className="login-sub">
-                <h2>Signup</h2>
-                <label htmlFor="">
-                    Enter your FirstName: <input type="text" name="" id="" />
-                </label>
-                <label htmlFor="">
-                    Enter your LastName: <input type="text" name="" id="" />
-                </label>
-                <label htmlFor="">
-                    Enter your email ID: <input type="email" name="" id="" />
-                </label>
-                <label htmlFor="">
-                    Enter your password: <input type="password" name="" id="" />
-                </label>
-                <label htmlFor="">
-                    Re-enter your password: <input type="password" />
-                </label>
-                <button type="submit">Sign Up</button>
-                <Link to="/login"> Already have an account? Login.</Link>
-            </form>
-            
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useContext, useState } from "react";
+import { productContext } from "../contexts/ProductContProvider";
 
-        </div>
-    )
+export default function Signup() {
+    const [user, setUser] = useState({});
+    const { signUpHandler } = useContext(productContext);
+  function handleInput(e) {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function notify(event) {
+    event.preventDefault();
+    toast.success("Signed up successfully!", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+  return (
+    <div className="login-parent">
+      <form action="" className="login-sub">
+        <h2 style={{ color: "var(--primary-color)" }}>Signup</h2>
+        <label htmlFor="" className="email">
+          Enter your FirstName:{" "}
+          <input
+            type="text"
+            name="firstName"
+            id=""
+            className="email-inp"
+            placeholder="Shubh"
+            onChange={(e) => handleInput(e)}
+          />
+        </label>
+        <label htmlFor="" className="email">
+          Enter your LastName:{" "}
+          <input
+            type="text"
+            name="lastName"
+            id=""
+            className="email-inp"
+            placeholder="Arya"
+            onChange={(e) => handleInput(e)}
+          />
+        </label>
+        <label htmlFor="" className="email">
+          Enter your email ID:{" "}
+          <input
+            type="email"
+            name="email"
+            id=""
+            className="email-inp"
+            placeholder="example@gmail.com"
+            onChange={(e) => handleInput(e)}
+          />
+        </label>
+        <label htmlFor="" className="pw">
+          Enter your password:{" "}
+          <input
+            type="password"
+            name="password"
+            id=""
+            className="pw-inp"
+            placeholder="shubh@123"
+            onChange={(e) => handleInput(e)}
+          />
+        </label>
+        <label htmlFor="" className="pw">
+          Re-enter your password:{" "}
+          <input type="password" className="pw-inp" placeholder="shubh@123" />
+        </label>
+        <button
+          type="submit"
+          className="signin-btn"
+          onClick={(e) => {
+            notify(e);
+            signUpHandler(user);
+          }}
+        >
+          Sign Up
+        </button>
+        <Link to="/login" className="signup-link">
+          {" "}
+          Already have an account? Login.
+        </Link>
+      </form>
+    </div>
+  );
 }
