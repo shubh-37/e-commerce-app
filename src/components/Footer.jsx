6dd35cd40648fca458e5f1a725ebb0bd148f0e22
shubh-37 <shubh.arya37@gmail.com>
2023-06-05 03:02:44 +0530

    added a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,29 @@
-export default function Footer(){
-    
+import "../css/footer.css";
+
+export default function Footer() {
+  return (
+    <footer class="footer">
+      <div class="footer-heading">Connect With Us </div>
+      <ul class="footer list-non-bullet">
+        <li class="list-item-inline">
+          <a href="https://github.com/shubh-37" class="link">
+            GitHub
+          </a>
+        </li>
+        <li class="list-item-inline">
+          <a href="https://twitter.com/shubh_37" class="link">
+            Twitter
+          </a>
+        </li>
+        <li class="list-item-inline">
+          <a
+            href="https://www.linkedin.com/in/shubh-arya-08b54b200/"
+            class="link"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
