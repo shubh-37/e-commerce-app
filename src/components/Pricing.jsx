a69da8491fa1a9d2163ebc05fc42ecca4364ae5a
shubh-37 <shubh.arya37@gmail.com>
2023-06-05 03:05:04 +0530

    error message feat

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,13 +1,48 @@
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-export default function Pricing({finalPrice, checker}) {
+export default function Pricing({ finalPrice, checker, radioCheck }) {
+  function orderHandler() {
+    if (radioCheck) {
+      toast.success("order placed successfully");
+    } else {
+      toast.error("Please select an address to continue", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  }
   return (
     <div className="pricing">
       <h3>Price Details</h3>
       <p>Price: {finalPrice}</p>
       <p>Delivery charges: 100</p>
       <p>Total amount: {finalPrice + 100}</p>
-      <button className="checkout">{checker ? <Link to="/checkout" style={{textDecoration: "none", color: "black"}}>Checkout</Link>: <Link to="/checkout" style={{textDecoration: "none", color: "black"}}>Place order</Link>}</button>
+      {checker ? (
+        <button className="checkout">
+          <Link
+            to="/checkout"
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            Checkout
+          </Link>
+        </button>
+      ) : (
+        <button onClick={() => orderHandler()} className="checkout">
+          <Link
+            to="/checkout"
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            Place order
+          </Link>
+        </button>
+      )}
     </div>
   );
 }
