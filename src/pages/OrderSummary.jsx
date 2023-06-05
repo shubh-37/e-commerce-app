786df4a2a56839da519dd981e64ca96cde10058f
shubh-37 <shubh.arya37@gmail.com>
2023-06-05 14:47:24 +0530

    added a new page to show order summary

---
+++ src/pages/OrderSummary.jsx
@@ -0,0 +1,29 @@
+import { useLocation } from "react-router-dom";
+
+export default function OrderSummary() {
+  
+  const location = useLocation();
+  const cart = location.state.data;
+  console.log(location.state, "hello")
+  // console.log(cart);
+  const finalPrice = cart?.reduce(
+    (acc, item) => (acc = acc + item.price * item.qty),
+    0
+  );
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Order Summary</h1>
+      <ul>
+        {cart?.map((item) => (
+          <li>
+            <h3>Order Name: {item.title}</h3>
+            <p>Quantity: {item.qty}</p>
+            <p>Price: Rs. {item.price}</p>
+          </li>
+        ))}
+      </ul>
+      <h4>Total Order price: {finalPrice + 100}</h4>
+      <h3>Thank you for 'booking' with us xD</h3>
+    </div>
+  );
+}
