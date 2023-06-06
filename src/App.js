f3f705fa451955486d192507c917fa00faa6bc9f
shubh-37 <shubh.arya37@gmail.com>
2023-06-06 15:24:54 +0530

    added a class of main content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {Routes, Route} from "react-router-dom";
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 import Mockman from "mockman-js";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -11,26 +11,29 @@ import Product from "./pages/SingleProductPage";
 import Login from "./pages/LoginPage";
 import Signup from "./pages/SignUpPage";
 import AddressPage from "./pages/AddressPage";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import OrderSummary from "./pages/OrderSummary";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/mockman" element={<Mockman />} />
-        <Route path="/products" element={<ProductListing />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/product/:prodId" element={<Product />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/checkout" element={<AddressPage />} />
-        <Route path="/summary" element={<OrderSummary />} />
-      </Routes>
-      <Footer />  
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/mockman" element={<Mockman />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/:prodId" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/checkout" element={<AddressPage />} />
+          <Route path="/summary" element={<OrderSummary />} />
+        </Routes>
+      </div>
+
+      <Footer />
       <ToastContainer />
     </div>
   );
