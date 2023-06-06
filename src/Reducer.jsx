4947bcf690a213817d4161e46a88f5f52ee06b72
shubh-37 <shubh.arya37@gmail.com>
2023-06-06 19:40:35 +0530

    added the clear filter logic and identation

--- src/Reducer.jsx
+++ src/Reducer.jsx
@@ -1,50 +1,71 @@
-export default function reducer(state, action){
-    switch(action.type){
-        case "ADD_PRODUCTS": return {
-            ...state,
-            allProducts: action.payload,
-            refData: action.payload
-        }
-        case "CATEGORIES" : return {
-            ...state,
-            categories: action.payload
-        }
-        case "ADD_TO_CART" : return {
-            ...state,
-            cartItems: action.payload
-        }
-        case "ADD_TO_WISHLIST": return{
-            ...state,
-            wishlistItems: action.payload
-        }
-        case "SORT": return {
-            ...state,
-            sortPrice: action.payload
-        }
-        case "RATING": return {
-            ...state,
-            rating: action.payload
-        }
-        case "CHECKBOX": {
-            return {
-                ...state,
-                category: state.category.includes(action.payload) ? state.category.filter((item) => item !== action.payload) : [...state.category, action.payload]
-            }
-        }
-        case "SEARCH" : return{
-            ...state,
-            searchBar: action.payload
-        }
-        case "REMOVE_ALL" : return {
-            ...state,
-            cartItems: action.payload
-        }
-        case "PRICE": return {
-            ...state,
-            price: Number(action.payload)
-        }
-        default : return {
-            ...state
-        }
+export default function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_PRODUCTS":
+      return {
+        ...state,
+        allProducts: action.payload,
+        refData: action.payload,
+      };
+    case "CATEGORIES":
+      return {
+        ...state,
+        categories: action.payload,
+      };
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cartItems: action.payload,
+      };
+    case "ADD_TO_WISHLIST":
+      return {
+        ...state,
+        wishlistItems: action.payload,
+      };
+    case "SORT":
+      return {
+        ...state,
+        sortPrice: action.payload,
+      };
+    case "RATING":
+      return {
+        ...state,
+        rating: action.payload,
+      };
+    case "CHECKBOX": {
+      return {
+        ...state,
+        category: state.category.includes(action.payload)
+          ? state.category.filter((item) => item !== action.payload)
+          : [...state.category, action.payload],
+      };
     }
+    case "SEARCH":
+      return {
+        ...state,
+        searchBar: action.payload,
+      };
+    case "REMOVE_ALL":
+      return {
+        ...state,
+        cartItems: action.payload,
+      };
+    case "PRICE":
+      return {
+        ...state,
+        price: Number(action.payload),
+      };
+    case "CLEAR":
+      return {
+        ...state,
+        sortPrice: action.payload.sortPrice,
+        rating: action.payload.rating,
+        searchBar: action.payload.searchBar,
+        price: action.payload.price,
+        category: action.payload.category,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
 }
