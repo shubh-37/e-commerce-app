4dd68074eace84e0e9835ca89564b679338d0ac5
shubh-37 <shubh.arya37@gmail.com>
2023-06-07 02:14:39 +0530

    rendering userProfile if person is logged in

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,51 +1,88 @@
-import { useContext } from "react"
-import { productContext } from "../contexts/ProductContProvider"
-import  {Link} from "react-router-dom";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import "../css/login.css";
 import { toast } from "react-toastify";
+import { authContext } from "../contexts/AuthProvider";
+import UserProfile from "../components/UserProfile";
 
-export default function Login(){
-    const { testLogin } = useContext(productContext);
-    function notify(val){
-        if(val === "t"){
-            toast.success("Login successful as test user!", {
-                position: "bottom-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-              });
-        }else{
-            toast.success("Login successful!", {
-                position: "bottom-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-              });
-        }
+export default function Login() {
+  const { loginTestUser, isLogin, userProfile } = useContext(authContext);
+  function notify(val) {
+    if (val === "t") {
+      toast.success("Login successful as test user!", {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else {
+      toast.success("Login successful!", {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
-    return (
+  }
+  return (
+    <>
+      {isLogin ? (
+        <UserProfile userProfile={userProfile}/>
+      ) : (
         <div className="login-parent">
-            <div className="login-sub">   
-                <h2 style={{color: "var(--primary-color)"}}>Login</h2>
-                <label htmlFor="" className="email">
-                    Email Address <br/><input type="text" name="" id="" className="email-inp" placeholder="example@gmail.com"/>
-                </label>
-                <label htmlFor="" className="pw">
-                    Password <br/><input type="password" name="" id="" className="pw-inp" placeholder="shubh@123" />
-                </label>
-                <button className="signin-btn" onClick={() => {notify("u"); }}>Sign In</button>
-                <Link to="/signup" className="signup-link">Don't have an account? Sign Up.</Link>
-                <button onClick={() => {testLogin(); notify("t");}} className="test-btn">Login as test user</button>
-            </div>
-            
+          <div className="login-sub">
+            <h2 style={{ color: "var(--primary-color)" }}>Login</h2>
+            <label htmlFor="" className="email">
+              Email Address <br />
+              <input
+                type="text"
+                name=""
+                id=""
+                className="email-inp"
+                placeholder="example@gmail.com"
+              />
+            </label>
+            <label htmlFor="" className="pw">
+              Password <br />
+              <input
+                type="password"
+                name=""
+                id=""
+                className="pw-inp"
+                placeholder="shubh@123"
+              />
+            </label>
+            <button
+              className="signin-btn"
+              onClick={() => {
+                notify("u");
+              }}
+            >
+              Sign In
+            </button>
+            <Link to="/signup" className="signup-link">
+              Don't have an account? Sign Up.
+            </Link>
+            <button
+              onClick={() => {
+                loginTestUser();
+                notify("t");
+              }}
+              className="test-btn"
+            >
+              Login as test user
+            </button>
+          </div>
         </div>
-    )
+      )}
+    </>
+  );
 }
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useContext, useState } from "react";
-import { productContext } from "../contexts/ProductContProvider";
+import { authContext } from "../contexts/AuthProvider";
+import UserProfile from "../components/UserProfile";
 
 export default function Signup() {
-    const [user, setUser] = useState({});
-    const { signUpHandler } = useContext(productContext);
+  const [user, setUser] = useState({});
+  const { signUpHandler, isLogin, userProfile } = useContext(authContext);
   function handleInput(e) {
     setUser({
       ...user,
@@ -26,73 +27,85 @@ export default function Signup() {
       theme: "light",
     });
   }
+  function clickHandler(e) {
+    notify(e);
+    signUpHandler(user);
+  }
   return (
-    <div className="login-parent">
-      <form action="" className="login-sub">
-        <h2 style={{ color: "var(--primary-color)" }}>Signup</h2>
-        <label htmlFor="" className="email">
-          Enter your FirstName:{" "}
-          <input
-            type="text"
-            name="firstName"
-            id=""
-            className="email-inp"
-            placeholder="Shubh"
-            onChange={(e) => handleInput(e)}
-          />
-        </label>
-        <label htmlFor="" className="email">
-          Enter your LastName:{" "}
-          <input
-            type="text"
-            name="lastName"
-            id=""
-            className="email-inp"
-            placeholder="Arya"
-            onChange={(e) => handleInput(e)}
-          />
-        </label>
-        <label htmlFor="" className="email">
-          Enter your email ID:{" "}
-          <input
-            type="email"
-            name="email"
-            id=""
-            className="email-inp"
-            placeholder="example@gmail.com"
-            onChange={(e) => handleInput(e)}
-          />
-        </label>
-        <label htmlFor="" className="pw">
-          Enter your password:{" "}
-          <input
-            type="password"
-            name="password"
-            id=""
-            className="pw-inp"
-            placeholder="shubh@123"
-            onChange={(e) => handleInput(e)}
-          />
-        </label>
-        <label htmlFor="" className="pw">
-          Re-enter your password:{" "}
-          <input type="password" className="pw-inp" placeholder="shubh@123" />
-        </label>
-        <button
-          type="submit"
-          className="signin-btn"
-          onClick={(e) => {
-            notify(e);
-            signUpHandler(user);
-          }}
-        >
-          Sign Up
-        </button>
-        <Link to="/login" className="signup-link">
-          {" "}
-          Already have an account? Login.
-        </Link>
-      </form>
-    </div>
+    <>
+      {isLogin ? (
+        <UserProfile userProfile={userProfile}/>
+      ) : (
+        <div className="login-parent">
+          <form onSubmit={(e) => clickHandler(e)} className="login-sub">
+            <h2 style={{ color: "var(--primary-color)" }}>Signup</h2>
+            <label htmlFor="" className="email">
+              Enter your FirstName:{" "}
+              <input
+                type="text"
+                name="firstName"
+                id=""
+                className="email-inp"
+                placeholder="Shubh"
+                onChange={(e) => handleInput(e)}
+                required
+              />
+            </label>
+            <label htmlFor="" className="email">
+              Enter your LastName:{" "}
+              <input
+                type="text"
+                name="lastName"
+                id=""
+                className="email-inp"
+                placeholder="Arya"
+                onChange={(e) => handleInput(e)}
+                required
+              />
+            </label>
+            <label htmlFor="" className="email">
+              Enter your email ID:{" "}
+              <input
+                type="email"
+                name="email"
+                id=""
+                className="email-inp"
+                placeholder="example@gmail.com"
+                onChange={(e) => handleInput(e)}
+                required
+              />
+            </label>
+            <label htmlFor="" className="pw">
+              Enter your password:{" "}
+              <input
+                type="password"
+                name="password"
+                id=""
+                className="pw-inp"
+                placeholder="shubh@123"
+                onChange={(e) => handleInput(e)}
+                required
+              />
+            </label>
+            <label htmlFor="" className="pw">
+              Re-enter your password:{" "}
+              <input
+                type="password"
+                className="pw-inp"
+                placeholder="shubh@123"
+                required
+              />
+            </label>
+            <button type="submit" className="signin-btn">
+              Sign Up
+            </button>
+            <Link to="/login" className="signup-link">
+              {" "}
+              Already have an account? Login.
+            </Link>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
