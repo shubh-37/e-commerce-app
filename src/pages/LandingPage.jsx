4941d9d3b2b13d490c313fb512caf26b07412e65
shubh-37 <shubh.arya37@gmail.com>
2023-06-07 05:03:54 +0530

    added a main image and a button to view all products

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,32 +1,46 @@
 import { useContext } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { productContext } from "../contexts/ProductContProvider";
-import "../css/landingpage.css"
+import "../css/landingpage.css";
 
-export default function LandingPage(){
-    const { state, showCategoryProd, isLoading } = useContext(productContext);
-    return (
-        <div className="parent">
-            <h1>{isLoading ? "Fetching products for you...": ""}</h1>
-            <ul className="sub-parent">
-                {
-                    state?.categories.map(item => (
-                        <li key={item.id} className="child" >
-                            <div className ="img-container" style={{backgroundImage: `url(${item.src})`}}>
-                                <div className="transparent">
-                                    <h1>{item.categoryName}</h1>
-                                    <p>{item.description}</p>
-                                    <button onClick={() => showCategoryProd(item.categoryName)} className="submit-btn">
-                                        <Link to ="/products" className="link">View Books</Link>
-                                    </button>
-                                </div>
-                                 
-                            </div>
-                            
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    )
+export default function LandingPage() {
+  const { state, showCategoryProd } = useContext(productContext);
+  return (
+    <div className="parent">
+    <div className="main-img">
+    <img
+        src="https://images2.imgbox.com/2f/b4/kAxjfFqv_o.jpg"
+        style={{ maxWidth: "100%", height: "auto" }}
+        alt="prod-img"
+        className="landing-img"
+      />
+      <h4 className="allProd-heading">View all products</h4>
+      <button className="allProd-btn">Books</button>
+    </div>
+      
+      <ul className="sub-parent">
+        {state?.categories.map((item) => (
+          <li key={item.id} className="child">
+            <div
+              className="img-container"
+              style={{ backgroundImage: `url(${item.src})` }}
+            >
+              <div className="transparent">
+                <h1>{item.categoryName}</h1>
+                <p>{item.description}</p>
+                <button
+                  onClick={() => showCategoryProd(item.categoryName)}
+                  className="submit-btn"
+                >
+                  <Link to="/products" className="link">
+                    View Books
+                  </Link>
+                </button>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
