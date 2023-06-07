229dcacbb4ff4b921274bed09f3702dad64dc073
shubh-37 <shubh.arya37@gmail.com>
2023-06-07 13:53:12 +0530

    added a checker for isLogin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,37 @@
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { productContext } from "../contexts/ProductContProvider";
 import "../css/header.css";
+import { authContext } from "../contexts/AuthProvider";
 
-export default function Header(){
-    const {searchHandler, state} = useContext(productContext);
-    return(
-        <div className="header">
-            <div >
-                <NavLink to ="/" className="heading-home"><h3 >Bookworm's Stop</h3></NavLink>
-            </div>
-            <div className="search">
-                <input onChange={(e) => searchHandler(e)} type="text" name="" id="" placeholder="Search"/>
-            </div>
-            <div className="icons">
-                    <NavLink to="/cart" className="fa fa-shopping-cart cart"><span>{state?.cartItems?.length}</span></NavLink>
-                    <NavLink to="/wishlist" className="fa fa-heart cart"><span>{state?.wishlistItems?.length}</span></NavLink>
-                    <NavLink to="/login" className="fa fa-user"></NavLink>
-            </div>
-        </div>
-    )
+export default function Header() {
+  const { searchHandler, state } = useContext(productContext);
+  const { isLogin } = useContext(authContext);
+  return (
+    <div className="header">
+      <div>
+        <NavLink to="/" className="heading-home">
+          <h3>Booksque</h3>
+        </NavLink>
+      </div>
+      <div className="search">
+        <input
+          onChange={(e) => searchHandler(e)}
+          type="text"
+          name=""
+          id=""
+          placeholder="Search"
+        />
+      </div>
+      <div className="icons">
+        <NavLink to="/cart" className="fa fa-shopping-cart cart">
+          {isLogin ? <span>{state?.cartItems?.length}</span> : <></>}
+        </NavLink>
+        <NavLink to="/wishlist" className="fa fa-heart cart">
+          {isLogin ? <span>{state?.wishlistItems?.length}</span> : <></>}
+        </NavLink>
+        <NavLink to="/login" className="fa fa-user"></NavLink>
+      </div>
+    </div>
+  );
 }
-
