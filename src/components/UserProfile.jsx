0b20be546d76ba3d5a3c877804f19d8d102368d5
shubh-37 <shubh.arya37@gmail.com>
2023-06-07 13:53:47 +0530

    imported stylesheets

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react"
 import { authContext } from "../contexts/AuthProvider"
-
+import "../css/userprofile.css";
 export default function UserProfile({userProfile}){
   const { removeToken } = useContext(authContext);
   return (
-    <div>
-      <h1>This is user profile</h1>
+    <div className="user-profile">
+      <h1>User profile</h1>
       <h3>Name: {`${userProfile?.firstName} ${userProfile?.lastName} `}</h3>
       <p>Email: {userProfile?.email}</p>
       <button onClick={() => removeToken()}>Logout</button>
