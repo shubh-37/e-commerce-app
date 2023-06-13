deaea6b504352699d0093eff990e7854910455c2
shubh-37 <shubh.arya37@gmail.com>
2023-06-14 01:40:25 +0530

    fixed a bug related to multiple item added to cart

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -11,7 +11,7 @@ import { authContext } from "../contexts/AuthProvider";
 export default function ProductListing() {
   const { ratedProd, handleCart, handleWishlist, state } =
     useContext(productContext);
-  const foundC = (prodId) => state?.cartItems?.some(({ id }) => id === prodId);
+  const foundC = (prodId) => state?.cartItems?.some(({ _id }) => _id === prodId);
   const foundWL = (prodName) =>
     state?.wishlistItems?.find(({ title }) => title === prodName);
 
@@ -69,15 +69,15 @@ export default function ProductListing() {
       </div>
       <ul className="product-list">
         {ratedProd?.map((item) => (
-          <li key={item.id} className="prod-item">
-            <Link to={`/product/${item.id}`}>
+          <li key={item._id} className="prod-item">
+            <Link to={`/product/${item._id}`}>
               <img src="https://picsum.photos/215/150" alt="prod-img" />
             </Link>
             <h3>Title: {item.title}</h3>
             <p>Price: Rs {item.price}</p>
             <p>Genre: {item.categoryName}</p>
             <p>Rating: {item.rating} stars</p>
-            {foundC(item.id) ? (
+            {foundC(item._id) ? (
               <Link to="/cart">
                 <button className="go-btn">Visit cart</button>
               </Link>
